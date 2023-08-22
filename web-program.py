c461bdfe64178a0f05f8df23995d106d3ec35059
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-08-22 21:32:14 +0300

    added web-program.py

---
+++ web-program.py
@@ -0,0 +1,32 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_page_data(self):
+        with open("sample.html", "r", encoding='utf-8') as f:
+            html__ = f.read()
+        return html__
+
+    def do_GET(self):
+        page_content = self.__get_page_data()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
